c5075df63930d6e2dbcd641da68a0f4c984adce0
Artur Gomes <arturgaollidev@outlook.com>
2024-02-26 12:58:27 -0300

    Criação do método de patch e delete. abstraindo funções de procura no database.

--- src/main/java/br/com/listtta/backend/controller/FiltersController.java
+++ src/main/java/br/com/listtta/backend/controller/FiltersController.java
@@ -2,6 +2,7 @@ package br.com.listtta.backend.controller;
 
 import br.com.listtta.backend.model.dto.generics.CreateNewFilterDto;
 import br.com.listtta.backend.model.dto.generics.FiltersDto;
+import br.com.listtta.backend.model.dto.generics.UpdateFilterDto;
 import br.com.listtta.backend.model.entities.Filters;
 import br.com.listtta.backend.service.FiltersService;
 import jakarta.validation.Valid;
@@ -24,6 +25,11 @@ public class FiltersController {
         return new ResponseEntity<>(filtersService.createNewFilter(createNewFilterDto), HttpStatus.CREATED);
     }
 
+    @PatchMapping("patch/{filterId}")
+    public ResponseEntity<Filters> patchFilter(@PathVariable Long filterId, @RequestBody UpdateFilterDto updateFilterDto) {
+        return new ResponseEntity<>(filtersService.patchFilter(filterId, updateFilterDto), HttpStatus.ACCEPTED);
+    }
+
     @GetMapping("list/{filterName}")
     public ResponseEntity<FiltersDto> getOneFilter(@PathVariable String filterName) {
         return new ResponseEntity<>(filtersService.getOneFilter(filterName), HttpStatus.OK);
@@ -32,6 +38,11 @@ public class FiltersController {
     @GetMapping("list/all")
     public ResponseEntity<List<FiltersDto>> getAllFilters() {
         return new ResponseEntity<>(filtersService.gerAllFilters(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("delete/{filterId}")
+    public ResponseEntity<String> deleteFilter(@PathVariable Long filterId) {
+        return new ResponseEntity<>(filtersService.deleteFilter(filterId), HttpStatus.ACCEPTED);
     }
 
 }
--- src/main/java/br/com/listtta/backend/service/FiltersService.java
+++ src/main/java/br/com/listtta/backend/service/FiltersService.java
@@ -1,13 +1,17 @@
 package br.com.listtta.backend.service;
 
+import br.com.listtta.backend.exceptions.UpdateFieldsException;
 import br.com.listtta.backend.model.dto.generics.CreateNewFilterDto;
 import br.com.listtta.backend.model.dto.generics.FiltersDto;
+import br.com.listtta.backend.model.dto.generics.UpdateFilterDto;
 import br.com.listtta.backend.model.entities.Filters;
 import br.com.listtta.backend.model.mapper.FiltersMapper;
 import br.com.listtta.backend.repository.FiltersRepository;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Var;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,21 +22,57 @@ public class FiltersService {
     private final FiltersRepository filtersRepository;
     private final FiltersMapper filtersMapper;
 
+    private Filters checkFilterInDatabaseByName(String filterName) {
+        Optional<Filters> checkFilterInDatabaseByName = filtersRepository.findByFilterName(filterName);
+
+        if (checkFilterInDatabaseByName.isPresent()) {
+            return checkFilterInDatabaseByName.get();
+        }
+        throw new RuntimeException("Filtro não encontrado!");
+    }
+
+    private Filters checkFilterInDatabaseById(Long filterId) {
+        Optional<Filters> checkFilterInDatabaseById = filtersRepository.findById(filterId);
+
+        if (checkFilterInDatabaseById.isPresent()) {
+            return checkFilterInDatabaseById.get();
+        }
+        throw new RuntimeException("Filtro não encontrado!");
+    }
+
     public Filters createNewFilter(CreateNewFilterDto createNewFilterDto) {
         return filtersRepository.save(filtersMapper.createNewFilterDtoToModel(createNewFilterDto));
     }
 
-    public FiltersDto getOneFilter(String filterName) {
-        Optional<Filters> checkFilterInDatabase = filtersRepository.findByFilterName(filterName);
-        if (checkFilterInDatabase.isPresent()) {
-            return filtersMapper.filtersModelToDto(checkFilterInDatabase.get());
-        } else {
-            throw new RuntimeException("Filtro Não Encontrado!");
+    public Filters patchFilter(Long filterId, UpdateFilterDto updateFilterDto) {
+        Filters checkedFilter = checkFilterInDatabaseById(filterId);
+        Filters fieldsToUpdate = filtersMapper.updateFilterDtoToModel(updateFilterDto);
+
+        for (Field field : Filters.class.getDeclaredFields()) {
+            field.setAccessible(true);
+
+            try {
+                if (field.get(fieldsToUpdate) != null && !field.get(fieldsToUpdate).equals(field.get(checkedFilter))) {
+                    field.set(checkedFilter, field.get(fieldsToUpdate));
+                }
+            } catch (IllegalAccessException e) {
+                throw new UpdateFieldsException("Não foi possível atualizar o filtro");
+            }
         }
+        return checkedFilter;
+    }
+
+    public FiltersDto getOneFilter(String filterName) {
+        return filtersMapper.filtersModelToDto(checkFilterInDatabaseByName(filterName));
     }
 
     public List<FiltersDto> gerAllFilters() {
         return filtersMapper.listFiltersModelToDto(filtersRepository.findAll());
     }
 
+    public String deleteFilter(Long filterId) {
+        checkFilterInDatabaseById(filterId);
+        filtersRepository.deleteById(filterId);
+        return "Filtro deleteado com sucesso!";
+    }
 }
