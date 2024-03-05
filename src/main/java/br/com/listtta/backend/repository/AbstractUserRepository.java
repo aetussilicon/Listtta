a8c123a5872d0d3483536a9179991a2fcfc9b466
Artur Gomes <arturgaollidev@outlook.com>
2024-03-05 15:12:54 -0300

    Abstraindo os repositórios.

---
+++ src/main/java/br/com/listtta/backend/repository/AbstractUserRepository.java
@@ -0,0 +1,13 @@
+package br.com.listtta.backend.repository;
+
+import br.com.listtta.backend.model.entities.AbstractUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@NoRepositoryBean
+public interface AbstractUserRepository<T> extends JpaRepository<T, UUID> {
+    Optional<AbstractUser> findUserByUsername(String username);
+}
