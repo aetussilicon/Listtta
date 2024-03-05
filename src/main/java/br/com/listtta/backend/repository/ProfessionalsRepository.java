2b7f51432644c3aad8a17e981d3733f05ef03bf4
Artur Gomes <arturgaollidev@outlook.com>
2024-03-05 15:12:54 -0300

    Abstraindo os repositórios.

--- src/main/java/br/com/listtta/backend/repository/ProfessionalsRepository.java
+++ src/main/java/br/com/listtta/backend/repository/ProfessionalsRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface ProfessionalsRepository extends JpaRepository<Professionals, UUID> {
+public interface ProfessionalsRepository extends AbstractUserRepository<Professionals> {
 }
