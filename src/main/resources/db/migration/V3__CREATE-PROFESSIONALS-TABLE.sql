11a5410ceddf2e0e14b78c7b9039d9cfc33f1a40
Artur <foxbat@fedora>
2024-03-16 17:14:08 -0300

    Recriando estrutura do banco de dados. Usuários sendo uma única tabela.

---
+++ src/main/resources/db/migration/V3__CREATE-PROFESSIONALS-TABLE.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS professionals (
+    user_id UUID PRIMARY KEY,
+    professional_type TEXT,
+    instagram_url VARCHAR(255),
+    skills BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (skills) REFERENCES filters(filter_id)
+);
--- src/main/resources/db/migration/V4__CREATE-PROFESSIONAL-SKILLS.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS professional_skills (
-    user_id UUID,
-    filter_id BIGINT,
-    PRIMARY KEY (user_id, filter_id),
-    FOREIGN KEY (user_id) REFERENCES professionals(user_id),
-    FOREIGN KEY (filter_id) REFERENCES filters(filter_id)
-);
