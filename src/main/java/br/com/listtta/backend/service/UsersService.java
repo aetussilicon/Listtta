85931cff64df8fcbb619ccf6ede2011c68b653de
Artur Gomes <arturgaollidev@outlook.com>
2024-03-22 12:19:08 -0300

    Atualizando classe de UserService para utilizar os métodos de consulta.

--- src/main/java/br/com/listtta/backend/service/UsersService.java
+++ src/main/java/br/com/listtta/backend/service/UsersService.java
@@ -1,20 +1,18 @@
 package br.com.listtta.backend.service;
 
-import br.com.listtta.backend.exceptions.UserNotFound;
-import br.com.listtta.backend.model.dto.ProfessionalsSignupDtoComplement;
+import br.com.listtta.backend.model.dto.professionals.ProfessionalsSignupDtoComplement;
+import br.com.listtta.backend.model.dto.users.UsersDto;
 import br.com.listtta.backend.model.dto.users.UsersSignupDto;
-import br.com.listtta.backend.model.entities.ProfessionalDetails;
 import br.com.listtta.backend.model.entities.Users;
 import br.com.listtta.backend.model.enums.UserRoles;
 import br.com.listtta.backend.model.mapper.ProfessionalsMapper;
 import br.com.listtta.backend.model.mapper.UsersMapper;
 import br.com.listtta.backend.repository.ProfessionalsRepository;
 import br.com.listtta.backend.repository.UsersRepository;
+import br.com.listtta.backend.util.FindUsersMethods;
 import br.com.listtta.backend.util.validation.CPFValidatorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,22 +24,15 @@ public class UsersService {
     private final UsernameGenerateService usernameGenerateService;
     private final CPFValidatorService validatorService;
     private final ProfessionalsMapper professionalsMapper;
-
-    private void checkUserInDatabaseByTaxNumberAndThrowError(String taxNumber) {
-        Optional<Users> checkingUser = usersRepository.findUserByTaxNumber(taxNumber);
-
-        if (checkingUser.isPresent()) {
-            throw new RuntimeException("Usuário já cadastrado.");
-        }
-    }
+    private final FindUsersMethods findUsersMethods;
 
     public Users createNewUser(UsersSignupDto usersSignupDto) {
         usersSignupDto.setTaxNumber(
                 validatorService.cpfValidation(usersSignupDto.getTaxNumber()));
 
-        checkUserInDatabaseByTaxNumberAndThrowError(usersSignupDto.getTaxNumber());
+        findUsersMethods.findUserByTaxNumberAndThrowErro(usersSignupDto.getTaxNumber());
 
-        usersSignupDto.setUsername(
+       usersSignupDto.setUsername(
                 usernameGenerateService.usernameGenerator(usersSignupDto.getFullName()));
 
         Users newUser = mapper.usersSignupDto(usersSignupDto);
@@ -61,4 +52,7 @@ public class UsersService {
         return newUser;
     }
 
+    public UsersDto getUser(String username) {
+        return mapper.userModelToDto(findUsersMethods.findUserByUsername(username));
+    }
 }
