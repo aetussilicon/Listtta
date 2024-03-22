a34fb5fe533879a606143391419b210bd16f19ea
Artur Gomes <arturgaollidev@outlook.com>
2024-03-22 18:57:57 -0300

    Métodos de listagem em mapper de profissional

--- src/main/java/br/com/listtta/backend/repository/UsersRepository.java
+++ src/main/java/br/com/listtta/backend/repository/UsersRepository.java
@@ -1,14 +1,15 @@
 package br.com.listtta.backend.repository;
 
 import br.com.listtta.backend.model.entities.Users;
+import br.com.listtta.backend.model.enums.UserRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UsersRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findUserByUsername(String username);
     Optional<Users> findUserByTaxNumber(String taxNumber);
+    List<Users> findByRole(UserRoles userRoles);
 }
