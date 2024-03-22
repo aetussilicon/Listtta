1bf2cfc13944176bd9c8229f76d4c62849dbbf51
Artur <foxbat@Cosmic>
2024-03-22 12:17:57 -0300

    Criação de classe para fazer as consultas no banco de dados por Username, Id e cpf/cnpj

---
+++ src/main/java/br/com/listtta/backend/util/FindUsersMethods.java
@@ -0,0 +1,49 @@
+package br.com.listtta.backend.util;
+
+import br.com.listtta.backend.exceptions.ProfessionalDetailsNotFoundException;
+import br.com.listtta.backend.exceptions.UserAlreadyInDatabaseException;
+import br.com.listtta.backend.exceptions.UserNotFound;
+import br.com.listtta.backend.model.entities.ProfessionalDetails;
+import br.com.listtta.backend.model.entities.Users;
+import br.com.listtta.backend.repository.ProfessionalsRepository;
+import br.com.listtta.backend.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class FindUsersMethods {
+
+    private final UsersRepository usersRepository;
+    private final ProfessionalsRepository professionalsRepository;
+
+
+    public void findUserByTaxNumberAndThrowErro(String taxNumber) throws UserAlreadyInDatabaseException {
+        Optional<Users> checkingUser = usersRepository.findUserByTaxNumber(taxNumber);
+
+        if (checkingUser.isPresent()) {
+            throw new UserAlreadyInDatabaseException();
+        }
+    }
+
+
+    public Users findUserByUsername(String username) {
+        Optional<Users> checkingUser = usersRepository.findUserByUsername(username);
+
+        if (checkingUser.isPresent()) {
+            return checkingUser.get();
+        } throw new UserNotFound();
+    }
+
+    public ProfessionalDetails findProfessionalByUser(Users users) {
+        Optional<ProfessionalDetails> checkingDetails = professionalsRepository.findProfessionalByUsers(users);
+
+        if (checkingDetails.isPresent()) {
+            return checkingDetails.get();
+        } throw new ProfessionalDetailsNotFoundException();
+
+    }
+
+}
