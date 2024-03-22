fe19ce5317d059f985656ef773877bb68d21bfc8
Artur Gomes <arturgaollidev@outlook.com>
2024-03-22 18:55:54 -0300

    Métodos do controller de get e update.

--- src/main/java/br/com/listtta/backend/controller/ProfessionalsController.java
+++ src/main/java/br/com/listtta/backend/controller/ProfessionalsController.java
@@ -1,14 +1,15 @@
 package br.com.listtta.backend.controller;
 
 import br.com.listtta.backend.model.dto.professionals.ProfessionalsDto;
+import br.com.listtta.backend.model.dto.professionals.ProfessionalsUpdateDto;
+import br.com.listtta.backend.model.entities.ProfessionalDetails;
 import br.com.listtta.backend.service.ProfessionalsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("professionals")
@@ -17,9 +18,21 @@ public class ProfessionalsController {
 
     private final ProfessionalsService professionalsService;
 
+    @PatchMapping
+    @RequestMapping("update/{username}")
+    public ResponseEntity<ProfessionalDetails> updateDetails(@PathVariable String username, ProfessionalsUpdateDto professionalsUpdateDto) {
+        return new ResponseEntity<>(professionalsService.updateProfessionalDetails(username, professionalsUpdateDto), HttpStatus.ACCEPTED);
+    }
+
     @GetMapping
     @RequestMapping("list/{username}")
     public ResponseEntity<ProfessionalsDto> getProfessional(@PathVariable String username) {
         return new ResponseEntity<>(professionalsService.getProfessional(username), HttpStatus.OK);
+    }
+
+    @GetMapping
+    @RequestMapping("list/all")
+    public ResponseEntity<List<ProfessionalsDto>> getAllProfessionals() {
+        return new ResponseEntity<>(professionalsService.listAllProfessionals(), HttpStatus.OK);
     }
 }
