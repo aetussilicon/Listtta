68665bb1ad072bd9c69d232bf72eae801d211281
Artur Gomes <arturgaollidev@outlook.com>
2024-03-25 12:04:50 -0300

    Criando auth controller.

--- src/main/java/br/com/listtta/backend/controller/AuthController.java
+++ src/main/java/br/com/listtta/backend/controller/AuthController.java
@@ -1,12 +1,17 @@
 package br.com.listtta.backend.controller;
 
+import br.com.listtta.backend.model.dto.authentication.LoginDto;
+import br.com.listtta.backend.model.dto.authentication.LoginResponseDto;
 import br.com.listtta.backend.model.dto.users.UsersSignupDto;
 import br.com.listtta.backend.model.entities.Users;
 import br.com.listtta.backend.service.UsersService;
+import br.com.listtta.backend.util.infra.security.TokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UsersService usersService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    @PostMapping
+    @RequestMapping("login")
+    public ResponseEntity login(@RequestBody @Valid LoginDto loginDto) {
+       var usernamePassword = new UsernamePasswordAuthenticationToken(loginDto.email(), loginDto.password());
+       var auth = this.authenticationManager.authenticate(usernamePassword);
+
+       var token = tokenService.generateToken((Users) auth.getPrincipal());
+
+       return ResponseEntity.ok(new LoginResponseDto(token));
+    }
 
     @PostMapping
     @RequestMapping("signup")
