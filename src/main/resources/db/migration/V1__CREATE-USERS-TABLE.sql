66e383c35dc8fe58c96f70af944e0c72c5c7336a
Artur Oliveira <arturgaollidev@outlook.com>
2024-04-05 20:18:52 -0300

    Removendo campo de Address do tabela. Criada nova tabela de user_address para comportar o UUID e UserTag do usuário.

--- src/main/resources/db/migration/V1__CREATE-USERS-TABLE.sql
+++ src/main/resources/db/migration/V1__CREATE-USERS-TABLE.sql
@@ -1,12 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
    user_id UUID PRIMARY KEY UNIQUE NOT NULL,
    full_name VARCHAR(90) NOT NULL,
-   user_tag VARCHAR(10) NOT NULL,
+   user_tag VARCHAR(10) NOT NULL UNIQUE,
    email VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
    role TEXT NOT NULL,
    tax_number VARCHAR(20) NOT NULL,
-   phone_number VARCHAR(20) NOT NULL,
-   user_address BIGINT NOT NULL,
-   FOREIGN KEY (user_address) REFERENCES users_address(address_id)
+   phone_number VARCHAR(20) NOT NULL
 );
