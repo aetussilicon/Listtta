60bcfacbcee2a56cc8f55b883f2e8f14098add68
Artur Oliveira <arturgaollidev@outlook.com>
2024-04-05 20:21:43 -0300

    Criando tabela de posts do blog.

--- src/main/resources/db/migration/V6__CREATE-BLOG-TABLE.sql
+++ src/main/resources/db/migration/V6__CREATE-BLOG-TABLE.sql
@@ -2,9 +2,12 @@ CREATE SEQUENCE blog_seq;
 
 CREATE TABLE IF NOT EXISTS blog (
     post_id BIGINT PRIMARY KEY NOT NULL UNIQUE,
-    author UUID NOT NULL,
+    author_id UUID NOT NULL,
+    author_user_tag VARCHAR(10) NOT NULL,
+    author_name VARCHAR(90) NOT NULL,
     created_date TIMESTAMP NOT NULL,
     post_category VARCHAR(255) NOT NULL,
     post_content TEXT NOT NULL,
-    FOREIGN KEY (author) REFERENCES users(user_id)
+    FOREIGN KEY (author_id) REFERENCES users(user_id),
+    FOREIGN KEY (author_user_tag) REFERENCES users(user_tag)
 );
