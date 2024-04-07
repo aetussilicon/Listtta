ad4b75d193f0242e31fee64c5367f19a1616fb32
Artur Oliveira <artrugaollidev@outlook.com>
2024-04-07 10:13:56 -0300

    Otimização dos imports, Organização das classes instanciadas, Método de cadastro de endereço criado, Cadastro de Profissional criado, Método de patch atualizado.

--- src/main/java/br/com/listtta/backend/service/UsersService.java
+++ src/main/java/br/com/listtta/backend/service/UsersService.java
@@ -1,69 +1,65 @@
 package br.com.listtta.backend.service;
 
-import br.com.listtta.backend.exceptions.UpdateFieldsException;
-import br.com.listtta.backend.model.dto.address.NewUserAddressDto;
-import br.com.listtta.backend.model.dto.address.UpdateUserAddressDto;
-import br.com.listtta.backend.model.dto.professionals.ProfessionalsSignupDtoComplement;
 import br.com.listtta.backend.model.dto.users.UsersDto;
 import br.com.listtta.backend.model.dto.users.UsersSignupDto;
 import br.com.listtta.backend.model.dto.users.UsersUpdateDto;
-import br.com.listtta.backend.model.entities.Address;
 import br.com.listtta.backend.model.entities.Users;
 import br.com.listtta.backend.model.enums.UserRoles;
-import br.com.listtta.backend.model.mapper.AddressMapper;
-import br.com.listtta.backend.model.mapper.ProfessionalsMapper;
 import br.com.listtta.backend.model.mapper.UsersMapper;
-import br.com.listtta.backend.repository.AddressRepository;
-import br.com.listtta.backend.repository.ProfessionalsRepository;
 import br.com.listtta.backend.repository.UsersRepository;
 import br.com.listtta.backend.util.FindUsersMethods;
 import br.com.listtta.backend.util.validation.CPFValidatorService;
+import br.com.listtta.backend.util.validation.Patcher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UsersService {
 
+    //Mappers
     private final UsersMapper mapper;
+
+    //Repositórios
     private final UsersRepository usersRepository;
-    private final ProfessionalsRepository professionalsRepository;
+
+    //Services
+    private final AddressService addressService;
+    private final ProfessionalsService professionalsService;
+
+    //Métodos extras
     private final UsernameGenerateService usernameGenerateService;
     private final CPFValidatorService validatorService;
-    private final ProfessionalsMapper professionalsMapper;
     private final FindUsersMethods findUsersMethods;
-    private final AddressMapper addressMapper;
-    private final AddressRepository addressRepository;
 
+    //Método de cadastro de usuários.
     public Users createNewUser(UsersSignupDto usersSignupDto) {
-        usersSignupDto.setTaxNumber(
-                validatorService.cpfValidation(usersSignupDto.getTaxNumber()));
 
+        //Valida o CPF do usuário e formata
+        usersSignupDto.setTaxNumber(validatorService.cpfValidation(usersSignupDto.getTaxNumber()));
+
+        //Verifica se não já não existe um usuário com esse CPF.
         findUsersMethods.findUserByTaxNumberAndThrowErro(usersSignupDto.getTaxNumber());
 
-       usersSignupDto.setUserTag(
-                usernameGenerateService.usernameGenerator(usersSignupDto.getFullName()));
+        //Cria uma UserTag -- MARCADO PARA REMOÇÃO
+       usersSignupDto.setUserTag(usernameGenerateService.usernameGenerator(usersSignupDto.getFullName()));
 
+       //Criptografa senha do usuário.
        usersSignupDto.setPassword(new BCryptPasswordEncoder().encode(usersSignupDto.getPassword()));
 
+       //Mapeia o usuário para entidade e salva no banco de dados.
         Users newUser = mapper.usersSignupDto(usersSignupDto);
-
         usersRepository.save(newUser);
 
-//        if (usersSignupDto.getRole() == UserRoles.PROFESSIONAL) {
-//            ProfessionalsSignupDtoComplement complement = new ProfessionalsSignupDtoComplement();
-//
-//            complement.setUsers(newUser);
-//            complement.setType(usersSignupDto.getType());
-//            complement.setInstagramUrl(usersSignupDto.getInstagramUrl());
-//
-//            professionalsRepository.save(professionalsMapper.professionalsDetailsDtoToModel(complement));
-//        }
+        //Após salvar, salva também o endereço do usuário.
+        addressService.createNewUserAddress(usersSignupDto);
+
+        if (usersSignupDto.getRole() == UserRoles.PROFESSIONAL) {
+            professionalsService.createNewProfessionalDetals(usersSignupDto);
+        }
 
         return newUser;
     }
@@ -72,44 +68,12 @@ public class UsersService {
         Users userToUpdate = findUsersMethods.findUserByUserTag(userTag);
         Users updateFields = mapper.updateDtoToModel(usersUpdateDto);
 
-        for(Field field : Users.class.getDeclaredFields()) {
-            field.setAccessible(true);
-
-            try {
-                if (field.get(updateFields) != null && !field.get(updateFields).equals(field.get(userToUpdate))) {
-                    field.set(userToUpdate, field.get(updateFields));
-                }
-            } catch (IllegalAccessException e) {
-                throw new UpdateFieldsException("Não foi possível atualizar o usuário");
-            }
+        try {
+            Patcher.patch(userToUpdate, updateFields);
+            usersRepository.save(userToUpdate);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
-
-        NewUserAddressDto address = new NewUserAddressDto();
-       address.setAddressId(userToUpdate.getUserAddress().getAddressId().longValue());
-
-
-
-//        Optional<Address> userAddress = addressRepository.findById(userAddressId);
-//        if (userAddress.isPresent()) {
-//            UpdateUserAddressDto updateAddress = new UpdateUserAddressDto();
-//            updateAddress.setCity(updateFields.getUserAddress().getCity());
-//            updateAddress.setDistrict(updateFields.getUserAddress().getDistrict());
-//            updateAddress.setState(updateFields.getUserAddress().getState());
-//            updateAddress.setPostalCode(updateFields.getUserAddress().getPostalCode());
-//
-//            for (Field field : Address.class.getDeclaredFields()) {
-//                field.setAccessible(true);
-//
-//                try {
-//                    if (field.get(updateAddress) != null && !field.get(updateAddress).equals(field.get(userAddress.get()))) {
-//                        field.set(userAddress.get(), field.get(updateAddress));
-//                    }
-//                } catch (IllegalAccessException e) {
-//                    throw new RuntimeException();
-//                }
-//            }
-
-//        }
 
         return userToUpdate;
     }
