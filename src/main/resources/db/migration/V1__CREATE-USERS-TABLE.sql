9dd5fff00a583a273a9f49fff13f7f18b5c55fca
Artur Oliveira <artrugaollidev@outlook.com>
2024-04-17 14:35:34 -0300

    Atualização das migrations: 	Remoção das tabelas do blog e fotos de perfil.

--- src/main/resources/db/migration/V1__CREATE-USERS-TABLE.sql
+++ src/main/resources/db/migration/V1__CREATE-USERS-TABLE.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
    user_id UUID PRIMARY KEY UNIQUE NOT NULL,
    full_name VARCHAR(90) NOT NULL,
-   user_tag VARCHAR(10) NOT NULL UNIQUE,
+   user_tag VARCHAR(20) NOT NULL UNIQUE,
+   profile_picture BYTEA,
    email VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
    role TEXT NOT NULL,
    tax_number VARCHAR(20) NOT NULL,
-   phone_number VARCHAR(20) NOT NULL
+   phone_number VARCHAR(20) NOT NULL,
+   created_date TIMESTAMP
 );
