1cab82a0fe40bf5c6e6a1283d985517f3cfb3c16
Artur Oliveira <arturgaollidev@outlook.com>
2024-05-04 23:35:59 -0300

    Otimização de imports

--- src/main/java/br/com/listtta/backend/repository/ProfessionalsRepository.java
+++ src/main/java/br/com/listtta/backend/repository/ProfessionalsRepository.java
@@ -1,7 +1,7 @@
 package br.com.listtta.backend.repository;
 
 import br.com.listtta.backend.model.entities.ProfessionalDetails;
-import br.com.listtta.backend.model.entities.Users;
+import br.com.listtta.backend.model.entities.users.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
