458fe8a6ce9eda902bac4b284aa6e722346cf5a1
Artur Oliveira <arturgaollidev@outlook.com>
2024-05-09 13:02:07 -0300

    Update ProfessionalViewRepository.java

--- src/main/java/br/com/listtta/backend/repository/ProfessionalViewRepository.java
+++ src/main/java/br/com/listtta/backend/repository/ProfessionalViewRepository.java
@@ -1,6 +1,6 @@
 package br.com.listtta.backend.repository;
 
-import br.com.listtta.backend.model.entities.ProfessionalView;
+import br.com.listtta.backend.model.entities.Professionals.ProfessionalView;
 import br.com.listtta.backend.model.enums.ProfessionalsType;
 import br.com.listtta.backend.model.enums.UserGender;
 import org.springframework.data.jpa.repository.JpaRepository;
