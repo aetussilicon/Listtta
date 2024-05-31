afaee9fc7fbfbe09005c07cd5e3d5768a87a9a59
Artur Oliveira <arturgaollidev@outlook.com>
2024-05-31 00:40:38 -0300

    Blog deletado
    Entidades, DTOs, Repositório, Service e controller de Blogs deletado
    --Marcado para remoção

--- src/main/java/br/com/listtta/backend/repository/BlogPostsRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.listtta.backend.repository;
-
-import br.com.listtta.backend.model.entities.blog.BlogPosts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogPostsRepository extends JpaRepository<BlogPosts, Long> {
-}
--- src/main/java/br/com/listtta/backend/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.listtta.backend.repository;
-
-import br.com.listtta.backend.model.entities.blog.BlogPosts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<BlogPosts, Long> {
-    
-}
