2109c245bbf39edccb268bfe3138f294a6f07105
Artur Oliveira <arturgaollidev@outlook.com>
2024-06-13 23:23:07 -0300

    Refatorando getProfessional
    getProfessional refatorado para usar ControllersResponse.

--- src/main/java/br/com/listtta/backend/controller/ProfessionalsController.java
+++ src/main/java/br/com/listtta/backend/controller/ProfessionalsController.java
@@ -1,9 +1,11 @@
 package br.com.listtta.backend.controller;
 
+import br.com.listtta.backend.exceptions.UserNotFound;
 import br.com.listtta.backend.model.dto.professionals.ProfessionalsDTO;
 import br.com.listtta.backend.model.dto.professionals.ProfessionalsUpdateDTO;
 import br.com.listtta.backend.model.entities.Professionals.ProfessionalDetails;
 import br.com.listtta.backend.service.ProfessionalsService;
+import br.com.listtta.backend.util.validation.ControllersResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("professionals")
@@ -18,19 +21,30 @@ import java.util.List;
 public class ProfessionalsController {
 
     private final ProfessionalsService professionalsService;
+    private final ControllersResponse responses;
 
+    //Update de profissionais
     @PatchMapping
     @RequestMapping("update/{puid}")
     public ResponseEntity<ProfessionalDetails> updateDetails(@PathVariable String puid,@RequestBody @Valid ProfessionalsUpdateDTO professionalsUpdateDto) {
         return new ResponseEntity<>(professionalsService.updateProfessionalDetails(puid, professionalsUpdateDto), HttpStatus.ACCEPTED);
     }
 
+    //Get um profissional
     @GetMapping
     @RequestMapping("list/{puid}")
-    public ResponseEntity<ProfessionalsDTO> getProfessional(@PathVariable String puid) {
-        return new ResponseEntity<>(professionalsService.getProfessional(puid), HttpStatus.OK);
+    public ResponseEntity<Map<String, Object>> getProfessional(@PathVariable String puid) {
+        try {
+            ProfessionalsDTO gettingProfessional = professionalsService.getProfessional(puid);
+            return new ResponseEntity<>(responses.controllersResponse(gettingProfessional, null), HttpStatus.OK);
+        } catch (UserNotFound e) {
+            return new ResponseEntity<>(responses.controllersResponse(null, e), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(responses.controllersResponse(null, e), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    //Listar todos os profissionais
     @GetMapping
     @RequestMapping("list/all")
     public ResponseEntity<List<ProfessionalsDTO>> getAllProfessionals() {
