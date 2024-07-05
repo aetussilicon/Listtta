a2579979dca09ee51e306b6277455ff58be2900f
Artur Oliveira <arturgaollidev@outlook.com>
2024-07-05 22:01:27 +0000

    Testando CORS

--- src/main/java/br/com/listtta/backend/util/infra/security/CorsConfiguration.java
+++ src/main/java/br/com/listtta/backend/util/infra/security/CorsConfiguration.java
@@ -11,6 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://89.116.74.28:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
         System.out.println("CORS configuration applied");
     }
