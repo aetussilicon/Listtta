baa7ec11d932d3f73b2b556532fbc807cb06c6a3
Artur Oliveira <arturgaollidev@outlook.com>
2024-07-13 18:43:02 -0300

    Atributo request removido (desnecessário)

--- src/main/java/br/com/listtta/backend/exceptions/GlobalFilesExceptionHandler.java
+++ src/main/java/br/com/listtta/backend/exceptions/GlobalFilesExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.Map;
 
@@ -18,12 +17,12 @@ public class GlobalFilesExceptionHandler {
     private final ControllersResponse response;
 
     @ExceptionHandler(FileToBigException.class)
-    public ResponseEntity<Map<String, Object>> handleFileToBigException (FileToBigException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleFileToBigException (FileToBigException exception) {
         return new ResponseEntity<>(response.controllersResponse(null, exception), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MimeTypeNotAllowedException.class)
-    public ResponseEntity<Map<String, Object>> handleMimeTypeNotAllowedException (MimeTypeNotAllowedException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleMimeTypeNotAllowedException (MimeTypeNotAllowedException exception) {
         return new ResponseEntity<>(response.controllersResponse(null, exception), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/br/com/listtta/backend/exceptions/GlobalUsersExceptionHandler.java
+++ src/main/java/br/com/listtta/backend/exceptions/GlobalUsersExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.Map;
 
@@ -18,32 +17,32 @@ public class GlobalUsersExceptionHandler {
     private final ControllersResponse response;
 
     @ExceptionHandler(UserNotAllowedException.class)
-    public ResponseEntity<Map<String, Object>> handleUserNotAllowedException(UserNotAllowedException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleUserNotAllowedException(UserNotAllowedException exception) {
         return new ResponseEntity<>(response.controllersResponse(null, exception), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(CannotRegisterUserException.class)
-    public ResponseEntity<Map<String, Object>> handleCannotRegisterUserException(CannotRegisterUserException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleCannotRegisterUserException(CannotRegisterUserException exception) {
         return new ResponseEntity<>(response.controllersResponse(null, exception), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CannotUpdateUsersFieldsException.class)
-    public ResponseEntity<Map<String, Object>> handleCannotUpdateUsersFieldException(CannotUpdateUsersFieldsException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleCannotUpdateUsersFieldException(CannotUpdateUsersFieldsException exception) {
         return new ResponseEntity<>(response.controllersResponse(null, exception), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserAlreadyInDatabaseException.class)
-    public ResponseEntity<Map<String, Object>> handleUserAlredyInDatabaseException(UserAlreadyInDatabaseException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleUserAlredyInDatabaseException(UserAlreadyInDatabaseException exception) {
         return new ResponseEntity<>(response.controllersResponse(null, exception), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException exception) {
         return new ResponseEntity<>(response.controllersResponse(null, exception), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleGlobalException(Exception exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleGlobalException(Exception exception) {
         return new ResponseEntity<>(response.controllersResponse(null,exception), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
